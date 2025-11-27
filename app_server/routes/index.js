936c65288f5fa3b5610899b44e6c23688d7168cf
Omar Camara <oc22416002@utg.edu.gm>
2025-11-27 18:45:42 +0000

    Updated the page layout

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var ctrMain = require('../controllers/main');
+// var ctrMain = require('../controllers/main');
 
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
+
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Others pages */
+router.get('/about', ctrlOthers.about);
 
 var homepageController = function (req, res, next) {
   res.render('index', { title: 'Express' });
 };
 
 /* GET home page. */
-router.get('/', homepageController);
-router.get('/', ctrMain.index);
+//router.get('/', homepageController);
+//router.get('/', ctrMain.index);
 
 module.exports = router;
