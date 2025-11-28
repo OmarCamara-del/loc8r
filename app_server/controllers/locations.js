f381c80004043b9dbb4c638788136c39a8db6fd6
Omar Camara <oc22416002@utg.edu.gm>
2025-11-28 12:31:29 +0000

    Adding the view template and fixing bootstrap styling

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,6 +8,6 @@ module.exports.locationInfo = function(req, res) {
 };
 
 /* GET 'Add review' page */
-module.exports.addReview = function(req, res) {
-    res.render('index', {title: 'Add review' });
+module.exports.reviewForm = function(req, res) {
+    res.render('location-review-form', {title: 'Add review' });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -9,7 +9,7 @@ var ctrlOthers = require('../controllers/others');
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/review/new', ctrlLocations.reviewForm);
 
 /* Others pages */
 router.get('/about', ctrlOthers.about);
